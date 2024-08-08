c3ddaae2c7e67301629bd134207d579ec71f8d63
Yani201 <yanetmekuria22@gmail.com>
2024-08-08 11:02:25 +0300

    refactoring Task 5

--- Task 5/task-manager-api-mongoDB/controller/task_controller.go
+++ Task 5/task-manager-api-mongoDB/controller/task_controller.go
@@ -1,1 +1,99 @@
-package controller 
+package controller
+
+import (
+	"errors"
+	"net/http"
+	"task-manager-api-mongoDB/data"
+	"task-manager-api-mongoDB/model"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func GetTasks(ctx *gin.Context) {
+	tasks, err := data.GetAllTasks()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve tasks"})
+		return
+	}
+
+	if len(tasks) == 0 {
+		ctx.IndentedJSON(http.StatusNotFound, gin.H{"error": "No tasks found"})
+		return
+	}
+
+	ctx.IndentedJSON(http.StatusOK, gin.H{"Tasks": tasks})
+}
+
+func GetTaskByID(ctx *gin.Context) {
+	id := ctx.Param("id")
+	task, err := data.GetTaskByID(id)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			ctx.IndentedJSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve task"})
+		return
+	}
+
+	ctx.IndentedJSON(http.StatusOK, task)
+}
+
+func UpdateTask(ctx *gin.Context) {
+	id := ctx.Param("id")
+	var updateTask model.Task
+
+	if err := ctx.BindJSON(&updateTask); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := data.UpdateTask(id, updateTask)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			ctx.IndentedJSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update task"})
+		return
+	}
+
+	ctx.IndentedJSON(http.StatusOK, gin.H{"message": "Task Updated"})
+}
+
+func CreateTask(ctx *gin.Context) {
+	var newTask model.Task
+
+	if err := ctx.BindJSON(&newTask); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	taskID := primitive.NewObjectID()
+    newTask.ID = taskID.Hex()
+
+	err := data.CreateTask(newTask)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create task"})
+		return
+	}
+
+	ctx.IndentedJSON(http.StatusCreated, gin.H{"message": "Task created"})
+}
+
+func DeleteTask(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	err := data.DeleteTask(id)
+	if err != nil {
+        if errors.Is(err, mongo.ErrNoDocuments) || errors.Is(err, data.ErrTaskNotFound) {
+            ctx.IndentedJSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+            return
+        }
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete task"})
+		return
+	}
+
+	ctx.IndentedJSON(http.StatusOK, gin.H{"message": "Task removed"})
+}
--- Task 5/task-manager-api-mongoDB/data/task_service.go
+++ Task 5/task-manager-api-mongoDB/data/task_service.go
@@ -1,1 +1,128 @@
 package data
+
+import (
+	"context"
+	"errors"
+	"log"
+	"sync"
+	"task-manager-api-mongoDB/model"
+	
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var url = "mongodb://localhost:27017"
+var db *mongo.Database
+var once sync.Once
+
+func GetClient() *mongo.Database {
+	once.Do(func() {
+		clientOpt := options.Client().ApplyURI(url)
+		client, err := mongo.Connect(context.TODO(), clientOpt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = client.Ping(context.TODO(), nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		db = client.Database("taskmanager")
+	})
+	return db
+}
+
+
+func GetAllTasks() ([]model.Task, error) {
+	db := GetClient()
+	cursor, err := db.Collection("Task").Find(context.TODO(), bson.D{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(context.TODO())
+
+	var tasks []model.Task
+	for cursor.Next(context.Background()) {
+		var task model.Task
+		if err := cursor.Decode(&task); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func GetTaskByID(id string) (model.Task, error) {
+	db := GetClient()
+	var task model.Task
+	filter := bson.D{{Key: "_id", Value: id}}
+	err := db.Collection("Task").FindOne(context.TODO(), filter).Decode(&task)
+	if err != nil {
+		return model.Task{}, err
+	}
+
+	return task, nil
+}
+
+func UpdateTask(id string, updateTask model.Task) error {
+	db := GetClient()
+	filter := bson.D{{Key: "_id", Value: id}}
+	update := bson.D{}
+
+	if updateTask.Title != "" {
+		update = append(update, bson.E{Key: "$set", Value: bson.D{{Key: "title", Value: updateTask.Title}}})
+	}
+	if !updateTask.DueDate.IsZero() {
+		update = append(update, bson.E{Key: "$set", Value: bson.D{{Key: "due_date", Value: updateTask.DueDate}}})
+	}
+	if updateTask.Status != "" {
+		update = append(update, bson.E{Key: "$set", Value: bson.D{{Key: "status", Value: updateTask.Status}}})
+	}
+	if updateTask.Description != "" {
+		update = append(update, bson.E{Key: "$set", Value: bson.D{{Key: "description", Value: updateTask.Description}}})
+	}
+
+	result, err := db.Collection("Task").UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	if result.ModifiedCount == 0 {
+		return errors.New("task not found")
+	}
+
+	return nil
+}
+
+func CreateTask(newTask model.Task) error {
+	db := GetClient()
+	_, err := db.Collection("Task").InsertOne(context.TODO(), newTask)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+var ErrTaskNotFound = errors.New("task not found")
+func DeleteTask(id string) error {
+	db := GetClient()
+	filter := bson.D{{Key: "_id", Value: id}}
+	result, err := db.Collection("Task").DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return err
+	}
+
+	if result.DeletedCount == 0 {
+		return ErrTaskNotFound
+	}
+	
+
+	return nil
+}
